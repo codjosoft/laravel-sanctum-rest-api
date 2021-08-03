d48bb9d25cff8aa0427e577264b9a3700069fa5e
codjosoft <yeboah.determined.isaac@gmail.com>
2021-08-03 18:03:33 +0000

    Update web.php

--- sanctum-rest-api/routes/web.php
+++ sanctum-rest-api/routes/web.php
@@ -27,3 +27,8 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
+
+Route::get('/products', function () {
+    return Inertia::render('Product');
+})->name('product');
