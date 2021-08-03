262a6658e21f1e57b5dfe550e780534ecd6b331a
codjosoft <yeboah.determined.isaac@gmail.com>
2021-08-03 19:30:40 +0000

    Update UserAuthController.php

--- sanctum-rest-api/app/Http/Controllers/UserAuthController.php
+++ sanctum-rest-api/app/Http/Controllers/UserAuthController.php
@@ -9,5 +9,12 @@ use illuminate\Support\Facades\Hash;
 
 class UserAuthController extends Controller
 {
-    
+    public function Register(Request $request){
+    $fields = $request->validate([
+        'name' => 'required|max:255',
+        'email' => 'required|email|max:255|unique:users',
+        'password' => 'required|min:6|confirmed'
+        ]);
+    }
+
 }
